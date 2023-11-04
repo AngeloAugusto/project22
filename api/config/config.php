f1a848a732c50eaf5d72ce887e1fd6718280bfa7
AngeloAugusto <angelo.augusto2024@gmail.com>
2023-11-04 23:50:05 +0000

    First commit

---
+++ api/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'project22');
+define('DB_USER', 'project22_admin');
+define('DB_PASSWORD', 'project22!');
+
+define('DEBUG', true); // Altere para 'false' no ambiente de produção
---
+++ api/routes/routes.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once '../controllers/example_controller.php';
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+$uriSegments = explode('/', parse_url($uri, PHP_URL_PATH));
+
+$dbConnection = (new Database())->getConnection();
+$controller = new ExampleController($dbConnection);
+
+if ($uriSegments[1] === 'example') {
+    $id = isset($uriSegments[2]) ? intval($uriSegments[2]) : null;
+
+    switch ($requestMethod) {
+        case 'GET':
+            if ($id) {
+                $response = $controller->get($id);
+            } else {
+                $response = $controller->getAll();
+            }
+            break;
+        case 'POST':
+            $response = $controller->create();
+            break;
+        case 'PUT':
+            if ($id) {
+                $response = $controller->update($id);
+            } else {
+                $response = ['status' => 'error', 'message' => 'Invalid example ID'];
+            }
+            break;
+        case 'DELETE':
+            if ($id) {
+                $response = $controller->delete($id);
+            } else {
+                $response = ['status' => 'error', 'message' => 'Invalid example ID'];
+            }
+            break;
+        default:
+            $response = ['status' => 'error', 'message' => 'Invalid request method'];
+            break;
+    }
+} else {
+    $response = ['status' => 'error', 'message' => 'Invalid API endpoint'];
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
